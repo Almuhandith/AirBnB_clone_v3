f4b7c38243b5d6b6d295ac074a986c64bb7dae2f
Abdulsamad <almuhandith1497@gmail.com>
2023-05-01 09:24:59 +0100

    count instance types

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
+    """ Returns JSON """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """ Returns the number of each instance type """
+    stats = {}
+    stats['amenities'] = storage.count('Amenity')
+    stats['cities'] = storage.count('City')
+    stats['places'] = storage.count('Place')
+    stats['reviews'] = storage.count('Review')
+    stats['states'] = storage.count('State')
+    stats['users'] = storage.count('User')
+    return jsonify(stats)
